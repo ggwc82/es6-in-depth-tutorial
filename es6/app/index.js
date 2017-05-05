1f05a222e14bc66f6ab1b983146f431ae517ff7d
Godfrey Chiu <godfrey.chiu@hssmi.org>
2017-05-05 10:41:07 +0100

    implemented the script into a method with a readable output

--- es6/app/index.js
+++ es6/app/index.js
@@ -6,24 +6,28 @@ values of the frequency of the character occurance;
 //create the string
 let superstring = "ejhfjbfkjksfbjkesfjjkwavncjkvwijngjevrshfguvjwebwuhewiewqopefj";
 
-//split the string into an array of characters
-let lettersArray = superstring.split("");
+const countLetters = (string, whichLetter) => {
+	//split the string into an array of characters
+	let lettersArray = string.split("");
 
-//now create the map data structure
-let lettersMap = new Map();
+	//now create the map data structure
+	let lettersMap = new Map();
 
-//create the for loop to iterate over each item in the lettersArray
-for (var i=0; i < lettersArray.length; i++) {
-	//create the temp variable
-	let letter = lettersArray[i];
+	//create the for loop to iterate over each item in the lettersArray
+	for (var i=0; i < lettersArray.length; i++) {
+		//create the temp variable
+		let letter = lettersArray[i];
 
-	//create the if not in the lettersMap statement
-	if (!lettersMap.has(letter)) {
-		// create the key value pair and set to 1
-		lettersMap.set(letter, 1);
-	} else {
-		// increment the existing value by 1
-		lettersMap.set(letter, lettersMap.get(letter) + 1);
+		//create the if not in the lettersMap statement
+		if (!lettersMap.has(letter)) {
+			// create the key value pair and set to 1
+			lettersMap.set(letter, 1);
+		} else {
+			// increment the existing value by 1
+			lettersMap.set(letter, lettersMap.get(letter) + 1);
+		}
 	}
+	return `The letter ${whichLetter} occurs ${lettersMap.get(whichLetter)} times in ${superstring}`;
 }
-console.log(lettersMap);
+
+console.log(countLetters(superstring, "a"));
